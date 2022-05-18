5284787dac26f861e0eabd20ca312bacdf5fd7d7
BMilliet <39925357+BMilliet@users.noreply.github.com>
2022-05-18 14:10:54 -0300

    Delete icli directory

--- icli/bin/icli.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# load icli main module
-require_relative '../lib/icli'
-
-# load command runner
-require_relative '../lib/command_runner'
-
-CommandRunner.start ARGV
