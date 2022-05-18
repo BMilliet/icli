b9fc1b93e08ad1e45fc51f176fcdcc62b33e9a9c
BMilliet <39925357+BMilliet@users.noreply.github.com>
2022-05-18 14:10:36 -0300

    Delete tests directory

--- tests/mocks/project_model_mock.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ProjectModelMock
-  attr_accessor :targets
-
-  def initialize(targets:)
-    @targets = targets
-  end
-end
--- tests/test_runner.rb
+++
@@ -1,6 +0,0 @@
-require 'minitest/autorun'
-
-require_relative '../icli/lib/icli'
-
-Dir["#{__dir__}/mocks/*.rb"].each { |f| require_relative f }
-Dir["#{__dir__}/test_cases/*.rb"].each { |f| require_relative f }
